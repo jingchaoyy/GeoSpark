7d70f13b52c0330c5a664b2081e821c639088385
jingchaoyy <jcyangt@gmail.com>
2018-05-03 12:37:47 -0400

    adding main, debug needed

--- application/src/main/scala/edu/gmu/stc/vector/sparkshell/GeoSpark_JoinTest_V1.scala
+++ application/src/main/scala/edu/gmu/stc/vector/sparkshell/GeoSpark_JoinTest_V1.scala
@@ -17,54 +17,64 @@ import org.datasyslab.geospark.spatialOperator.JoinQuery
 
 object GeoSpark_JoinTest_V1 {
 
-  // Initiate SparkContext
-  val conf = new SparkConf()
-  conf.setAppName("GeoSpark-Join") // Change this to a proper name
-  conf.setMaster("local[*]") // Delete this if run in cluster mode
-  // Enable GeoSpark custom Kryo serializer
-  conf.set("spark.serializer", classOf[KryoSerializer].getName)
-  conf.set("spark.kryo.registrator", classOf[GeoSparkKryoRegistrator].getName)
-  val sc = new SparkContext(conf)
+  def main(args: Array[String]): Unit = {
 
+    // Initiate SparkContext
+    val conf = new SparkConf()
+    conf.setAppName("GeoSpark-Join") // Change this to a proper name
+    conf.setMaster("local[*]") // Delete this if run in cluster mode
+    // Enable GeoSpark custom Kryo serializer
+    conf.set("spark.serializer", classOf[KryoSerializer].getName)
+    conf.set("spark.kryo.registrator", classOf[GeoSparkKryoRegistrator].getName)
+//    val sc = new SparkContext(conf)
 
-  // Create two generic SpatialRDD from shp
-  val shapefileInputLocation1="data/Washington_DC/Soil_Type_by_Slope"
-  val shapefileInputLocation2="data/Washington_DC/Impervious_Surface_2015"
+    if (System.getProperty("os.name").equals("Mac OS X")) {
+      conf.setMaster("local[6]")
+    }
 
-  val sparkSession: SparkSession = SparkSession.builder()
-    .config("spark.serializer", classOf[KryoSerializer].getName)
-    .config("spark.kryo.registrator", classOf[GeoSparkKryoRegistrator].getName)
-    .master("local[*]")
-    .appName("GeoSpark-Join")
-    .getOrCreate()
+    val sc = new SparkContext(conf)
 
-  var spatialRDD1 = new SpatialRDD[Geometry]
-  spatialRDD1.rawSpatialRDD = ShapefileReader.readToGeometryRDD(sparkSession.sparkContext, shapefileInputLocation1)
+    // Create two generic SpatialRDD from shp
+    val shapefileInputLocation1 = "data/Washington_DC/Soil_Type_by_Slope"
+    val shapefileInputLocation2 = "data/Washington_DC/Impervious_Surface_2015"
 
-  var spatialRDD2 = new SpatialRDD[Geometry]
-  spatialRDD2.rawSpatialRDD = ShapefileReader.readToGeometryRDD(sparkSession.sparkContext, shapefileInputLocation2)
+    val sparkSession: SparkSession = SparkSession.builder()
+      .config("spark.serializer", classOf[KryoSerializer].getName)
+      .config("spark.kryo.registrator", classOf[GeoSparkKryoRegistrator].getName)
+      .master("local[*]")
+      .appName("GeoSpark-Join")
+      .getOrCreate()
 
-  // Transform the Coordinate Reference System
-//  var objectRDD = new PointRDD(sc, pointRDDInputLocation, pointRDDOffset, pointRDDSplitter, carryOtherAttributes)
-  val sourceCrsCode = "epsg:4326" // WGS84, the most common degree-based CRS
-  val targetCrsCode = "epsg:3857" // The most common meter-based CRS
-//  objectRDD.CRSTransform(sourceCrsCode, targetCrsCode)
-  spatialRDD1.CRSTransform(sourceCrsCode, targetCrsCode)
-  spatialRDD2.CRSTransform(sourceCrsCode, targetCrsCode)
+    var spatialRDD1 = new SpatialRDD[Geometry]
+    spatialRDD1.rawSpatialRDD = ShapefileReader.readToGeometryRDD(sparkSession.sparkContext, shapefileInputLocation1)
+    print("********* ", spatialRDD1.countWithoutDuplicates())
+    var spatialRDD2 = new SpatialRDD[Geometry]
+    spatialRDD2.rawSpatialRDD = ShapefileReader.readToGeometryRDD(sparkSession.sparkContext, shapefileInputLocation2)
+    print("********* ", spatialRDD2.countWithoutDuplicates())
+    // Transform the Coordinate Reference System
+    //  var objectRDD = new PointRDD(sc, pointRDDInputLocation, pointRDDOffset, pointRDDSplitter, carryOtherAttributes)
+    val sourceCrsCode = "epsg:4326" // WGS84, the most common degree-based CRS
+    val targetCrsCode = "epsg:3857" // The most common meter-based CRS
+    //  objectRDD.CRSTransform(sourceCrsCode, targetCrsCode)
+    //spatialRDD1.CRSTransform(sourceCrsCode, targetCrsCode)
+    //spatialRDD2.CRSTransform(sourceCrsCode, targetCrsCode)
 
-  // Read other attributes in an SpatialRDD
-  val rddWithOtherAttributes1 = spatialRDD1.rawSpatialRDD.rdd.map[String](f=>f.getUserData.asInstanceOf[String])
-  val rddWithOtherAttributes2 = spatialRDD2.rawSpatialRDD.rdd.map[String](f=>f.getUserData.asInstanceOf[String])
+    // Read other attributes in an SpatialRDD
+    val rddWithOtherAttributes1 = spatialRDD1.rawSpatialRDD.rdd.map[String](f => f.getUserData.asInstanceOf[String])
+    val rddWithOtherAttributes2 = spatialRDD2.rawSpatialRDD.rdd.map[String](f => f.getUserData.asInstanceOf[String])
 
-  val considerBoundaryIntersection = false // Only return gemeotries fully covered by each query window in queryWindowRDD
-  val usingIndex = false
+    val considerBoundaryIntersection = false // Only return gemeotries fully covered by each query window in queryWindowRDD
+    val usingIndex = false
 
-  spatialRDD1.analyze()
+    spatialRDD1.analyze()
 
-  spatialRDD1.spatialPartitioning(GridType.KDBTREE)
-  spatialRDD2.spatialPartitioning(spatialRDD1.getPartitioner)
+    spatialRDD1.spatialPartitioning(GridType.KDBTREE)
+    spatialRDD2.spatialPartitioning(spatialRDD1.getPartitioner)
 
-  val result = JoinQuery.SpatialJoinQuery(spatialRDD1, spatialRDD2, usingIndex, considerBoundaryIntersection)
+    val result = JoinQuery.SpatialJoinQuery(spatialRDD1, spatialRDD2, usingIndex, considerBoundaryIntersection)
+    print("******************** ", result.count())
+    sparkSession.stop()
+  }
 
 }
 
